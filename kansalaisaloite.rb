e647dc9def0e283ea01454a9215aab96c022517a
Iiro Uusitalo <iiro.uusitalo@solita.fi>
2013-03-01 22:12:33 +0200

    Ensimmäinen committi

---
+++ kansalaisaloite.rb
@@ -0,0 +1,51 @@
+#!/usr/bin/env ruby
+require 'open-uri'
+require 'twitter'
+require 'json'
+require 'mongo'
+
+include Mongo
+
+data = JSON.parse(open("https://www.kansalaisaloite.fi/api/v1/initiatives?&limit=50&minSupportCount=0").read)
+db = MongoClient.new("localhost", 27017).db("kansalaisaloite")
+coll = db["aloitteet"]
+
+Twitter.configure do |config|
+  config.consumer_key       = ""
+  config.consumer_secret    = ""
+  config.oauth_token        = ""
+  config.oauth_token_secret = ""
+end
+
+data.each do |obj|
+    if obj['primaryLanguage'] == "fi"
+        aloite = coll.find({"id" => obj['id']}).first
+        url = obj['id'].gsub("/api/v1/initiatives/", "/fi/aloite/")
+
+        if aloite.nil?
+            if obj['name']['fi'].length > 100
+                title = obj['name']['fi'][0...100] + "..."
+            else
+                title = obj['name']['fi']
+            end
+            
+            message = "#{title} - #{url}"
+            # puts message
+            Twitter.update(message)
+
+            coll.insert(obj)
+        else
+            uusiarvo = obj['totalSupportCount'].to_i / 5000
+            vanhaarvo = aloite['totalSupportCount'].to_i / 5000
+
+            if uusiarvo != vanhaarvo
+                message = "Aloite #{url} - saavutti #{uusiarvo*5}k kannatuksen rajan! #kansalaisaloite"
+                # puts message
+
+                Twitter.update(message)
+
+                coll.update({"id" => obj['id']}, {"$set" => {"totalSupportCount" => obj['totalSupportCount']}})
+            end
+        end
+    end
+end
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Kansalaisaloitebot
+Idea lähti kansalaisaloite.fi verkkopalvelun puuttumisesta vallan Twitteristä, joten ajattelin lomapäivänäni koodata moisen.
+
+Aluksi tarkoituksena oli tuoda uudet aloitteet twitterikansan tietouteen, mutta "Osallistumisympäristö-hankkeen kehittäjäyhteisö" Facebook groupissa Miro heitti villin idean että @aloite_bot voisi myös twiitata merkkipaaluista ja niiin sittnen kävi.
+
+Kansalaisaloitebot twitterissä: <a href="http://www.twitter.com/aloite_bot">@aloite_bot</a>
